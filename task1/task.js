d6493c009a7fc64be2aa570853f2f380eb8c6027
Rákosi Márton <marci@twinson.hu>
2024-09-25 12:24:11 +0200

    task 1 done

--- task1/task.js
+++ task1/task.js
@@ -1,3 +1,15 @@
 export function findByActorNameAndGenre(allMovies, allActors, actorName, genre) {
 
+    const actor = allActors.find(item => item.name === actorName);
+    const actorIndex = actor ? actor.id : -1;
+
+    const movies = allMovies.filter(movie => {
+        let returnStatement = false;
+        if (movie.actor_ids.includes(actorIndex) &&
+            movie.genres.includes(genre) ) {
+                returnStatement = true;
+        }
+        return returnStatement;
+    })
+    return movies.map(movie => movie.title);
 }
