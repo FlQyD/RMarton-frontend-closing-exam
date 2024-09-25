2098294caf9dd8df84ee606cae7faf0806e19c0b
Rákosi Márton <marci@twinson.hu>
2024-09-25 12:30:54 +0200

    task 2 done

--- task2/task.js
+++ task2/task.js
@@ -1,3 +1,13 @@
 export function findUniqueGenres(genres) {
+    const uniqueGenres = [];
 
+    genres.forEach(innerGenres => {
+        innerGenres.forEach(genre => {
+
+            if (!uniqueGenres.includes(genre)) uniqueGenres.push(genre)    
+
+        });
+    });
+
+    return uniqueGenres;
 }
